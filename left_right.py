c19da1452a5db47821add55200361b237b2d8fe9
philiptruong-im-hub <phlptrng@gmail.com>
2026-04-10 16:08:45 -0400

    Add files via upload

---
+++ left_right.py
@@ -0,0 +1,11 @@
+noomer1 = int(input("What is your first number?: "))
+noomer2 = int(input("What is your second number?: "))
+
+if noomer2 > noomer1:
+    print("Number 2 is bigger than number 1 :D")
+
+elif noomer2 == noomer1:
+    print("They are both the same, twin")
+
+else:
+    print("Number 1 is bigger than number 2 :D")
